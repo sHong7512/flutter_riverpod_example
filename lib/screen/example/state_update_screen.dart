d6e2b54043bf801e9786aaf1d7e07dc2d18768f4
SoonHong Kwon <ksh7512@uangel.com>
2022-10-18 15:07:47 +0900

    [Modify] StateUpdate Example

--- lib/screen/example/state_update_screen.dart
+++ lib/screen/example/state_update_screen.dart
@@ -5,15 +5,25 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final counterProvider = StateProvider<int>((ref) => 0);
 
 class StateUpdateScreen extends ConsumerWidget {
-  const StateUpdateScreen({Key? key}): super(key: key);
+  const StateUpdateScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('StateUpdate'),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           ref.read(counterProvider.notifier).update((state) => state + 1);
         },
+        child: const Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text(
+          '${ref.watch(counterProvider)}',
+          style: TextStyle(fontSize: 25),
+        ),
       ),
     );
   }
